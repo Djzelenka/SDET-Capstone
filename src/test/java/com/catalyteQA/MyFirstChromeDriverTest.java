42a4d1c0b8ffeb351e33b5d33e766d654f6058ad
Peter Por <petervpor@gmail.com>
2020-10-30 09:31:28 -0700

    Added a few tests

---
+++ src/test/java/com/catalyteQA/MyFirstChromeDriverTest.java
@@ -0,0 +1,30 @@
+package com.catalyteQA;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class MyFirstChromeDriverTest {
+
+    @Test //annotation allows the test runner know that this is a test
+    public void driverIsKing(){
+        // instantiate the Firefox driver
+        WebDriver driver = new ChromeDriver();
+
+        // tell the driver to navigate to the test page
+        driver.navigate().to("https://testpages.herokuapp.com/");
+
+        // assert that the title starts with Selenium
+        assertTrue(driver.getTitle().startsWith("Selenium"));
+        assertEquals("Title is correct", driver.getTitle(), "Selenium Test Pages");
+
+        // quit the driver
+        driver.close();
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/com/catalyteQA/MyFirstFirefoxDriverTest.java
@@ -0,0 +1,24 @@
+package com.catalyteQA;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstFirefoxDriverTest {
+
+    @Test //annotation allows the test runner know that this is a test
+    public void driverIsKing() {
+        // instantiate the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        // tell the driver to navigate to the test page
+        driver.navigate().to("https://testpages.herokuapp.com/");
+
+        // assert that the title starts with Selenium
+        Assert.assertTrue(driver.getTitle().startsWith("Selenium"));
+
+        // quit the driver
+        driver.quit();
+    }
+}
